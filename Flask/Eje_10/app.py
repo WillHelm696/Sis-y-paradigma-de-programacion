de2151d47def7f5e8405c07be6e463c453f3508c
root <root@localhost.localdomain>
2025-11-20 19:37:55 -0300

    Practico 10 v2

--- Flask/Eje_10/app.py
+++ Flask/Eje_10/app.py
@@ -6,12 +6,21 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 @app.route('/')
-def mostrar_formulario():
+def inicio():
     return render_template('Ejercicio_10.html')
 
 usuarios = {"pedro":"pedro_123","juan":"juan_123","jose":"jose_123","jesus":"jesus_321","lucas":"lucas_321"}
 
-@app.route('/logeo',methods=['POST','GET'])
+@app.route('/crgar_banco_xxx')
+def carga_operaciones_poa():
+    try:
+        esta_logueado = session['logueado']
+        return render_template("banco_xxx.html")
+
+    except KeyError:
+        return ("NO ESTA LOGUEADO")
+
+@app.route('/logueo', methods=["GET", "POST"])
 def mostrar_formulario():
     sessions['logueado']=False
     datos=request.get_json()
@@ -19,10 +28,14 @@ def mostrar_formulario():
     user= datos['user'].lower
     pasw=datos['pasw'].lower
 
-    
-
+    for usuario in usuario:
+        if user == usuario:
+            if  pasw == usuarios[user]:
+                sessions['logeado']=True    
     if sessions['logueado']:
         return render_template('banco_xxx.html')
+    else:
+        return "Usuario y  vontraseña incorreta"
 
 
 if __name__ == '__main__':
--- Flask/Eje_10/static/logeo.js
+++ Flask/Eje_10/static/logeo.js
@@ -1,8 +1,20 @@
-function inisiar_secision(){
+function iniciar_sesion(){
     data={
         user: document.getElementById("user").value,
         pasw: document.getElementById("pasw").value
     }
+    let msj = document.getElementById("mensaje")
+    let rta = ""
+
+    if(data.user.length <= 0 ){
+        rta += "Complte el campo de username <br>"
+    }if (data.pasw.length <= 0){
+        rta += "Complte el campo de password <br>"
+    }if (rta.length <= 0 ){
+        msj.innerHTML= rta
+        return
+    }
+    alert(data.user,data.pasw)
 
     $.ajax=({
         type: 'POST',
